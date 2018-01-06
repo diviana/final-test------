2a6ac2ad3c56af27367f08c1e6e9bc9b11eae023
IvayloZashev <ivaylo_zashev@yahoo.bg>
2018-01-06 17:10:43 +0200

    Final exam: Ivaylo Zashev's solutions

--- task2/script.js
+++ task2/script.js
@@ -3,24 +3,50 @@
 // задача 1 (4т.)
 // Сортирайте следния списък от числа във възходящ ред:
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a, b){
+    return a-b;
+});
 
+console.log("arr", arr);
 //...
 
 // задача 2 (4т.)
 // Напишете функция, която да изважда всички думи, с дължина над 4 символа от следния текст:
 var text = "The quick brown fox jumps over the lazy dog";
-
+var words = extractWord(text, 4);
+function extractWord(text, desiredlength) {
+    var wordsArr = text.split(" ");
+    var extracted;
+    extracted = wordsArr.filter(function (word) {
+        return word.length > desiredlength;
+    });
+    
+    console.log("extracted", extracted);
+    return extracted;
+}
 // ...
 
 // задача 3 (5т.)
 // Напишете код, който на всяка секунда закача към `#container` елемента следния html елемент: `<p>repetition is fun</p>`
+function appendPElement() {
+    var containerElement = document.querySelector("#container");
+    var childElement = document.createElement("p");
+    childElement.innerHTML = "repetition is fun";
+    containerElement.appendChild(childElement);
+}
 
-// ...
+window.setInterval(appendPElement, 1000);
+
+
 
 // задача 4 (6т.)
 // Направете така, че 2 секунди след зареждането на дадена страница, всички картинки в нея да се завъртят по вертикалната си ос (по Y)
 
-// ...
+//window.setTimeout( ,2000);
+//function rotateImage () {
+//    document.querySelectorAll("img");
+//    
+//}
 
 // задача 5 (6т.)
 // Напишете функция, която да сортира следния списък от обекти по полето `price`:
@@ -47,4 +73,3 @@ var items = [
   }
 ];
 
-// ...
---
+++ task3/js/kid.js
@@ -0,0 +1,8 @@
+function Kid (name, age, color, game, food) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.game = game;
+    this.food = food;
+}
+
